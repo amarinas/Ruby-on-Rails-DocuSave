c0e9e48fcc9f8d9342f5fe281c7681a5bf9edff1
amarinas <ali.marinas@icloud.com>
2017-04-22 10:04:12 -0700

    initial

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,25 @@
+class DocsController < ApplicationController
+  def index
+  end
+
+  def show
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def fin_doc
+    end
+
+    def doc_params
+    end
+end
